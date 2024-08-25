cde017009bed9a163a0d6f94b543082e536dbf82
Zile Xiao <zxiao@nextdoor.com>
2024-08-25 13:54:29 -0700

    add aerial support

---
+++ nvim/.config/nvim/lua/zxiao/plugins/aerial.lua
@@ -0,0 +1,40 @@
+return {
+	"stevearc/aerial.nvim",
+	opts = {},
+	-- Optional dependencies
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("aerial").setup({
+			-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+			attach_mode = "global",
+			backends = { "lsp", "treesitter", "markdown", "man" },
+			show_guides = true,
+			layout = {
+				resize_to_content = false,
+				win_opts = {
+					winhl = "Normal:NormalFloat,FloatBorder:NormalFloat,SignColumn:SignColumnSB",
+					signcolumn = "yes",
+					statuscolumn = " ",
+				},
+			},
+      -- stylua: ignore
+      guides = {
+        mid_item   = "├╴",
+        last_item  = "└╴",
+        nested_top = "│ ",
+        whitespace = "  ",
+      },
+			default_direction = "prefer_left",
+			on_attach = function(bufnr)
+				-- Jump forwards/backwards with '{' and '}'
+				vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+				vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+			end,
+		})
+		-- You probably also want to set a keymap to toggle aerial
+		vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
+	end,
+}
